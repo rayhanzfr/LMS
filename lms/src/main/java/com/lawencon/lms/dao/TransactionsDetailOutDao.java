f214c10214f82414965c3cd800004415a2c73412
BondhanJantriR <otaku.player23@gmail.com>
2021-12-13 14:34:09 +0700

    update dao

---
+++ lms/src/main/java/com/lawencon/lms/dao/TransactionsDetailOutDao.java
@@ -0,0 +1,7 @@
+package com.lawencon.lms.dao;
+
+import com.lawencon.lms.model.TransactionsDetailOut;
+
+public interface TransactionsDetailOutDao {
+	TransactionsDetailOut getById(String id) throws Exception;
+}
