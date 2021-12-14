d027bc4baf7067f3c4e88dc3fc811cc7608bb92a
Bahrul Faizi <bahrul.faizi@gmail.com>
2021-12-14 17:29:28 +0700

    convert to dto trans in

---
+++ lms/src/main/java/com/lawencon/lms/dto/transactionsin/GetByTransactionsInIdResDto.java
@@ -0,0 +1,23 @@
+package com.lawencon.lms.dto.transactionsin;
+
+public class GetByTransactionsInIdResDto {
+	private GetTransactionsInDataDto data;
+	private String message;
+
+	public GetTransactionsInDataDto getData() {
+		return data;
+	}
+
+	public void setData(GetTransactionsInDataDto data) {
+		this.data = data;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
