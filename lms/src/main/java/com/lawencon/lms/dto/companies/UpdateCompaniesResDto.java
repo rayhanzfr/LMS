f147ca737dccb16e7d834610076fd05b47a28f4e
Bahrul Faizi <bahrul.faizi@gmail.com>
2021-12-15 11:55:15 +0700

    add res sto companies, invoices, locations, status assets

---
+++ lms/src/main/java/com/lawencon/lms/dto/companies/UpdateCompaniesResDto.java
@@ -0,0 +1,23 @@
+package com.lawencon.lms.dto.companies;
+
+public class UpdateCompaniesResDto {
+	private Integer version;
+	private String message;
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
