3dfb8b5dadb989dda5b3049926a326a60981a5b7
Bahrul Faizi <bahrul.faizi@gmail.com>
2021-12-15 17:02:39 +0700

    add service annotation companies, invoices, locations, status assets, trans in

--- lms/src/main/java/com/lawencon/lms/service/impl/TransactionsInServiceImpl.java
+++ lms/src/main/java/com/lawencon/lms/service/impl/TransactionsInServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.lawencon.base.BaseServiceImpl;
 import com.lawencon.lms.dao.AssetsDao;
@@ -29,6 +30,7 @@ import com.lawencon.lms.model.TransactionsDetailIn;
 import com.lawencon.lms.model.TransactionsIn;
 import com.lawencon.lms.service.TransactionsInService;
 
+@Service
 public class TransactionsInServiceImpl extends BaseServiceImpl implements TransactionsInService {
 
 	@Autowired
