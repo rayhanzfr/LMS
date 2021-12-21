312138121ea06630c9189364cceca7504de9566a
BondhanJantriR <67788316+BondhanJantriR@users.noreply.github.com>
2021-12-21 09:39:14 +0700

    update

--- lms/src/main/java/com/lawencon/lms/controller/ItemsTypesController.java
+++ lms/src/main/java/com/lawencon/lms/controller/ItemsTypesController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lawencon.lms.dto.itemstypes.SaveItemsTypesResDto;
 import com.lawencon.lms.dto.itemstypes.UpdateItemsTypesResDto;
@@ -23,6 +25,8 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
+@RestController
+@RequestMapping("items-types")
 public class ItemsTypesController {
 
 	@Autowired
