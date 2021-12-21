062373975fad4ab09e18f92270e09ab038a620a4
rayhanzfr <rayhanzaf@gmail.com>
2021-12-21 13:11:43 +0700

    add validationusers at roles,permissions,items,items-brands,transactions-out

--- lms/src/main/java/com/lawencon/lms/service/impl/ItemsBrandsServiceImpl.java
+++ lms/src/main/java/com/lawencon/lms/service/impl/ItemsBrandsServiceImpl.java
@@ -3,13 +3,22 @@ package com.lawencon.lms.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.lawencon.lms.constant.EnumCode;
 import com.lawencon.lms.dao.ItemsBrandsDao;
+import com.lawencon.lms.dao.PermissionsDao;
+import com.lawencon.lms.dao.PermissionsRolesDao;
+import com.lawencon.lms.dao.RolesDao;
+import com.lawencon.lms.dao.UsersDao;
 import com.lawencon.lms.dto.itemsbrands.SaveItemsBrandsResDto;
 import com.lawencon.lms.dto.itemsbrands.UpdateItemsBrandsResDto;
 import com.lawencon.lms.model.ItemsBrands;
+import com.lawencon.lms.model.Permissions;
+import com.lawencon.lms.model.PermissionsRoles;
+import com.lawencon.lms.model.Roles;
+import com.lawencon.lms.model.Users;
 import com.lawencon.lms.service.ItemsBrandsService;
 
 @Service
@@ -18,77 +27,134 @@ public class ItemsBrandsServiceImpl extends BaseServiceLmsImpl implements ItemsB
 	@Autowired
 	private ItemsBrandsDao itemsBrandsDao;
 
+	@Autowired
+	private PermissionsDao permissionsDao;
+
+	@Autowired
+	private UsersDao usersDao;
+
+	@Autowired
+	private RolesDao rolesDao;
+
+	@Autowired
+	private PermissionsRolesDao permissionsRolesDao;
+
 	@Override
 	public SaveItemsBrandsResDto save(ItemsBrands itemsBrands) throws Exception {
-		SaveItemsBrandsResDto saveItemsBrandsResDto = new SaveItemsBrandsResDto();
-		try {
-			begin();
-			itemsBrands.setItemsBrandsCode(generateCode());
-			itemsBrands.setCreatedBy(getIdAuth());
-			itemsBrands = itemsBrandsDao.saveOrUpdate(itemsBrands);
-			commit();
-			saveItemsBrandsResDto.setId(itemsBrands.getId());
-			saveItemsBrandsResDto.setMsg("OK");
-		} catch (Exception e) {
-			e.printStackTrace();
-			rollback();
+		String permissionsCode = "PERMSN30";
+		Boolean validation = validationUsers(permissionsCode);
+		if (validation) {
+			SaveItemsBrandsResDto saveItemsBrandsResDto = new SaveItemsBrandsResDto();
+			try {
+				begin();
+				itemsBrands.setItemsBrandsCode(generateCode());
+				itemsBrands.setCreatedBy(getIdAuth());
+				itemsBrands = itemsBrandsDao.saveOrUpdate(itemsBrands);
+				commit();
+				saveItemsBrandsResDto.setId(itemsBrands.getId());
+				saveItemsBrandsResDto.setMsg("OK");
+			} catch (Exception e) {
+				e.printStackTrace();
+				rollback();
+			}
+			return saveItemsBrandsResDto;
 		}
-		return saveItemsBrandsResDto;
+		throw new Exception("Access Denied");
 	}
 
 	@Override
 	public UpdateItemsBrandsResDto update(ItemsBrands itemsBrands) throws Exception {
-		UpdateItemsBrandsResDto updateItemsBrandsResDto = new UpdateItemsBrandsResDto();
-		try {
-			ItemsBrands itemsBrandsDb = findByCode(itemsBrands.getItemsBrandsCode());
-			itemsBrands.setCreatedAt(itemsBrandsDb.getCreatedAt());
-			itemsBrands.setCreatedBy(itemsBrandsDb.getCreatedBy());
-			itemsBrands.setUpdatedBy(getIdAuth());
-			begin();
-			itemsBrands = itemsBrandsDao.saveOrUpdate(itemsBrands);
-			commit();
-			updateItemsBrandsResDto.setVersion(itemsBrands.getVersion());
-			updateItemsBrandsResDto.setMsg("OK");
-		} catch (Exception e) {
-			e.printStackTrace();
-			rollback();
+		String permissionsCode = "PERMSN31";
+		Boolean validation = validationUsers(permissionsCode);
+		if (validation) {
+			UpdateItemsBrandsResDto updateItemsBrandsResDto = new UpdateItemsBrandsResDto();
+			try {
+				ItemsBrands itemsBrandsDb = findByCode(itemsBrands.getItemsBrandsCode());
+				itemsBrands.setCreatedAt(itemsBrandsDb.getCreatedAt());
+				itemsBrands.setCreatedBy(itemsBrandsDb.getCreatedBy());
+				itemsBrands.setUpdatedBy(getIdAuth());
+				begin();
+				itemsBrands = itemsBrandsDao.saveOrUpdate(itemsBrands);
+				commit();
+				updateItemsBrandsResDto.setVersion(itemsBrands.getVersion());
+				updateItemsBrandsResDto.setMsg("OK");
+			} catch (Exception e) {
+				e.printStackTrace();
+				rollback();
+			}
+			return updateItemsBrandsResDto;
 		}
-		return updateItemsBrandsResDto;
+		throw new Exception("Access Denied");
 	}
 
 	@Override
 	public ItemsBrands findById(String id) throws Exception {
-		return itemsBrandsDao.findById(id);
+		String permissionsCode = "PERMSN29";
+		Boolean validation = validationUsers(permissionsCode);
+		if (validation)
+			return itemsBrandsDao.findById(id);
+		throw new Exception("Access Denied");
 	}
 
 	@Override
 	public List<ItemsBrands> findAll() throws Exception {
-		return itemsBrandsDao.findAll();
+		String permissionsCode = "PERMSN29";
+		Boolean validation = validationUsers(permissionsCode);
+		if (validation)
+			return itemsBrandsDao.findAll();
+		throw new Exception("Access Denied");
 	}
 
 	@Override
 	public ItemsBrands findByCode(String code) throws Exception {
-		return itemsBrandsDao.findByCode(code);
+		String permissionsCode = "PERMSN29";
+		Boolean validation = validationUsers(permissionsCode);
+		if (validation)
+			return itemsBrandsDao.findByCode(code);
+		throw new Exception("Access Denied");
 	}
 
 	@Override
 	public Boolean removeById(String id) throws Exception {
-		try {
-			begin();
-			boolean isDeleted = itemsBrandsDao.removeById(id);
-			commit();
+		String permissionsCode = "PERMSN32";
+		Boolean validation = validationUsers(permissionsCode);
+		if (validation) {
+			try {
+				begin();
+				boolean isDeleted = itemsBrandsDao.removeById(id);
+				commit();
 
-			return isDeleted;
-		} catch (Exception e) {
-			e.printStackTrace();
-			rollback();
-			throw new Exception(e);
-		}
+				return isDeleted;
+			} catch (Exception e) {
+				e.printStackTrace();
+				rollback();
+				throw new Exception(e);
+			}
+		}throw new Exception("Access Denied");
 	}
 
 	public String generateCode() throws Exception {
 		Integer increment = itemsBrandsDao.countData() + 1;
 		String code = EnumCode.ITEMBRANDS.getCode() + increment;
 		return code;
+	}
+
+	public Boolean validationUsers(String permissionsCode) throws Exception {
+		try {
+			Users users = usersDao.findById(getIdAuth());
+			Roles roles = rolesDao.findById(users.getRoles().getId());
+			Permissions permissions = permissionsDao.findByCode(permissionsCode);
+			List<PermissionsRoles> listPermissionsRoles = permissionsRolesDao.findAll();
+			for (int i = 0; i < listPermissionsRoles.size(); i++) {
+				if (listPermissionsRoles.get(i).getPermissions().getId() == permissions.getId()) {
+					if (listPermissionsRoles.get(i).getRoles().getId() == roles.getId()) {
+						return true;
+					}
+				}
+			}
+			return false;
+		} catch (NotFoundException e) {
+			throw new Exception(e);
+		}
 	}
 }
