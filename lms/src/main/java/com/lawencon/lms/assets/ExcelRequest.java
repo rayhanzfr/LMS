f449af17f3145a7f3347d2f181eb65496de0977e
BondhanJantriR <67788316+BondhanJantriR@users.noreply.github.com>
2021-12-31 17:45:28 +0700

    update

--- lms/src/main/java/com/lawencon/lms/assets/ExcelRequest.java
+++ lms/src/main/java/com/lawencon/lms/assets/ExcelRequest.java
@@ -18,92 +18,89 @@ import com.lawencon.lms.dto.assets.SaveAssetsReqDto;
 import liquibase.pro.packaged.ce;
 
 public class ExcelRequest {
-	
 
 	static String type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-	static String[] headers = {"items_code","invoices_code","statuses_assets_code","statuses_in_out_code","assets_expired"};
+	static String[] headers = { "items_code", "invoices_code", "statuses_assets_code", "statuses_in_out_code",
+			"assets_expired" };
 	static String sheets = "assets";
-	
+
 	public static boolean excelFormat(MultipartFile file) {
-		if(!type.equals(file.getContentType())) {
+		if (!type.equals(file.getContentType())) {
 			return false;
 		}
 		return true;
 	}
-	
-	public static List<SaveAssetsReqDto> excelToAssets (InputStream input){
+
+	public static List<SaveAssetsReqDto> excelToAssets(InputStream input) {
 		try {
 			Workbook workbook = new XSSFWorkbook(input);
-			
+
 			Sheet sheet = workbook.getSheet(sheets);
 			Iterator<Row> rows = sheet.iterator();
-			
+
 			List<SaveAssetsReqDto> assets = new ArrayList<SaveAssetsReqDto>();
 			int rowNum = 0;
-			while(rows.hasNext()) {
+			while (rows.hasNext()) {
 				Row row = rows.next();
-				
-				//cek header upload same with header
-				if(rowNum==0) {
+
+				// cek header upload same with header
+				if (rowNum == 0) {
 					rowNum++;
 					continue;
 				}
-				
-				//set data in cell into asset
-				else {
-					Iterator<Cell> cellInRow = row.iterator();
-					
-					SaveAssetsReqDto asset = new SaveAssetsReqDto();
-					
-					int cellIndex =0;
-					while(cellInRow.hasNext()) {
-					
-						String data = null;
-						Cell cell = cellInRow.next();
 
-						//change cell type to string
-						CellType type = cell.getCellType();
-						if(cellInRow!=null) {
-							if(type == CellType.STRING) {
-								data = cell.getStringCellValue();
-							}else if(type == CellType.NUMERIC) {
-								data = String.valueOf(cell.getNumericCellValue());
-							}else if(type == CellType.BOOLEAN) {
-								data = String.valueOf(cell.getBooleanCellValue());
-							}
-							else if(type==CellType.BLANK) {
-								data = null;
-							}
+				// set data in cell into asset
+				Iterator<Cell> cellInRow = row.iterator();
+
+				SaveAssetsReqDto asset = new SaveAssetsReqDto();
+
+				int cellIndex = 0;
+				while (cellInRow.hasNext()) {
+
+					String data = null;
+					Cell cell = cellInRow.next();
+
+					// change cell type to string
+					CellType type = cell.getCellType();
+					if (cellInRow != null) {
+						if (type == CellType.STRING) {
+							data = cell.getStringCellValue();
+						} else if (type == CellType.NUMERIC) {
+							data = String.valueOf(cell.getNumericCellValue());
+						} else if (type == CellType.BOOLEAN) {
+							data = String.valueOf(cell.getBooleanCellValue());
+						} else if (type == CellType.BLANK) {
+							data = null;
 						}
-						
-						switch(cellIndex) {
-						case 0:
-							asset.setItemsCode(data);
-							break;
-						case 1:
-							asset.setInvoicesCode(data);
-							break;
-						case 2:
-							asset.setStatusesAssetsCode(data);
-							break;
-						case 3:
-							asset.setStatusesInOutCode(data);
-							break;
-						case 4:
-							asset.setAssetsExpired(data);
-							break;
-						default :
-							break;
-						}
-						cellIndex++;
 					}
-					assets.add(asset);
+					
+					switch (cellIndex) {
+					case 0:
+						asset.setItemsCode(data);
+						break;
+					case 1:
+						asset.setInvoicesCode(data);
+						break;
+					case 2:
+						asset.setStatusesAssetsCode(data);
+						break;
+					case 3:
+						asset.setStatusesInOutCode(data);
+						break;
+					case 4:
+						asset.setAssetsExpired(data);
+						break;
+					default:
+						break;
+					}
+					cellIndex++;
 				}
+				assets.add(asset);
 			}
 			workbook.close();
 			return assets;
 		} catch (Exception e) {
-			throw new RuntimeException("fail to parse Excel file: "+e.getMessage());
+			throw new RuntimeException("fail to parse Excel file: " + e.getMessage());
 		}
 	}
 }
--- lms/src/main/java/com/lawencon/lms/dao/impl/CompaniesDaoImpl.java
+++ lms/src/main/java/com/lawencon/lms/dao/impl/CompaniesDaoImpl.java
@@ -30,66 +30,75 @@ public class CompaniesDaoImpl extends BaseDaoImpl<Companies> implements Companie
 	@Override
 	public Companies findByCode(String code) throws Exception {
 		StringBuilder sql = new StringBuilder();
-		sql.append(
-				" SELECT c.id, c.companies_code, c.companies_name, c.companies_phone, c.companies_address, c.files_id, c.version, c.created_at, c.created_by, c.updated_at, c.updated_by, c.is_active ");
-		sql.append(" FROM companies as c");
-		sql.append(" INNER JOIN files as f ON f.id = c.files_id ");
-		sql.append(" WHERE companies_code = :companies_code ");
-
-		Companies companies = null;
-
-		try {
-			Object resultQuery = createNativeQuery(sql.toString()).setParameter("companies_code", code)
-					.getSingleResult();
-
-			if (resultQuery != null) {
-				Object[] objArr = (Object[]) resultQuery;
-				companies = new Companies();
-
-				String id = objArr[0].toString();
-				String companiesCode = objArr[1].toString();
-				String companiesName = objArr[2].toString();
-				String companiesPhone = objArr[3].toString();
-				String companiesAddress = objArr[4].toString();
-				String filesId = objArr[5].toString();
-				Integer version = (Integer) objArr[6];
-				LocalDateTime createdAt = Timestamp.valueOf(objArr[7].toString()).toLocalDateTime();
-				String createdBy = objArr[8].toString();
-
-				if (objArr[9] != null) {
-					LocalDateTime updatedAt = Timestamp.valueOf(objArr[9].toString()).toLocalDateTime();
-					String updatedBy = objArr[10].toString();
-					companies.setUpdatedAt(updatedAt);
-					companies.setUpdatedBy(updatedBy);
-				}
-
-				Boolean isActive = Boolean.parseBoolean(objArr[11].toString());
-
-				Files files = new Files();
-				files.setId(filesId);
-				;
-
-				companies.setId(id);
-				companies.setCompaniesCode(companiesCode);
-				companies.setCompaniesName(companiesName);
-				companies.setCompaniesPhone(companiesPhone);
-				companies.setCompaniesAddress(companiesAddress);
-				companies.setFiles(files);
-				companies.setVersion(version);
-				companies.setCreatedAt(createdAt);
-				companies.setCreatedBy(createdBy);
-				companies.setIsActive(isActive);
-
-			}
-		} catch (NoResultException e) {
-			e.printStackTrace();
-			throw new NoResultException("Not Found");
-		} catch (NonUniqueResultException e) {
-			e.printStackTrace();
-			throw new NonUniqueResultException("Found more than one");
-		}
-
+		sql.append(" SELECT c ");
+		sql.append(" FROM Companies c ");
+		sql.append(" INNER JOIN FETCH c.files f ");
+		sql.append(" WHERE c.companiesCode= :code ");
+		
+		Companies companies = createQuery(sql.toString(),Companies.class)
+				.setParameter("code", code)
+				.getSingleResult();
 		return companies;
+//		sql.append(
+//				" SELECT c.id, c.companies_code, c.companies_name, c.companies_phone, c.companies_address, c.files_id, c.version, c.created_at, c.created_by, c.updated_at, c.updated_by, c.is_active ");
+//		sql.append(" FROM companies as c");
+//		sql.append(" INNER JOIN files as f ON f.id = c.files_id ");
+//		sql.append(" WHERE companies_code = :companies_code ");
+//
+//		Companies companies = null;
+//
+//		try {
+//			Object resultQuery = createNativeQuery(sql.toString()).setParameter("companies_code", code)
+//					.getSingleResult();
+//
+//			if (resultQuery != null) {
+//				Object[] objArr = (Object[]) resultQuery;
+//				companies = new Companies();
+//
+//				String id = objArr[0].toString();
+//				String companiesCode = objArr[1].toString();
+//				String companiesName = objArr[2].toString();
+//				String companiesPhone = objArr[3].toString();
+//				String companiesAddress = objArr[4].toString();
+//				String filesId = objArr[5].toString();
+//				Integer version = (Integer) objArr[6];
+//				LocalDateTime createdAt = Timestamp.valueOf(objArr[7].toString()).toLocalDateTime();
+//				String createdBy = objArr[8].toString();
+//
+//				if (objArr[9] != null) {
+//					LocalDateTime updatedAt = Timestamp.valueOf(objArr[9].toString()).toLocalDateTime();
+//					String updatedBy = objArr[10].toString();
+//					companies.setUpdatedAt(updatedAt);
+//					companies.setUpdatedBy(updatedBy);
+//				}
+//
+//				Boolean isActive = Boolean.parseBoolean(objArr[11].toString());
+//
+//				Files files = new Files();
+//				files.setId(filesId);
+//				;
+//
+//				companies.setId(id);
+//				companies.setCompaniesCode(companiesCode);
+//				companies.setCompaniesName(companiesName);
+//				companies.setCompaniesPhone(companiesPhone);
+//				companies.setCompaniesAddress(companiesAddress);
+//				companies.setFiles(files);
+//				companies.setVersion(version);
+//				companies.setCreatedAt(createdAt);
+//				companies.setCreatedBy(createdBy);
+//				companies.setIsActive(isActive);
+//
+//			}
+//		} catch (NoResultException e) {
+//			e.printStackTrace();
+//			throw new NoResultException("Not Found");
+//		} catch (NonUniqueResultException e) {
+//			e.printStackTrace();
+//			throw new NonUniqueResultException("Found more than one");
+//		}
+//
+//		return companies;
 	}
 
 	@Override
